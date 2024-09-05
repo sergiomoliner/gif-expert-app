8d4c573002fc912021ca0ccc339817af52d755f0
Sergio Moliner <sergio.molinermagana@plexus.es>
2024-09-05 17:26:06 +0200

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components"
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch'])
+
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+
+        setCategories([...categories, newCategory])
+    }
+
+    return (
+        <>
+            {/* Titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+            <AddCategory
+                onNewCategory={event => onAddCategory(event)}
+            />
+
+            {/* Listado */}
+            <ol>
+                {categories.map(category =>
+                (
+                    <GifGrid key={category} category={category} />
+                )
+                )}
+            </ol>
+        </>
+    )
+}
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const cleanValue = inputValue.trim();
+    if (cleanValue.length <= 1) return;
+    // onAddCategory((categories) => [inputValue, ...categories]);
+    onNewCategory(cleanValue)
+    setInputValue("");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Buscar gifs"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+      </form>
+    </>
+  );
+};
